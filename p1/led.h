233a44a47d7e55a5c57185d01c3eb6e59e94460a
granthoover <grant2hoover2@gmail.com>
2020-02-03 23:12:15 -0500

    Revert "Add p1 files"
    This reverts commit bf1db243691dfd3d67b2ea72d43c1d49370677b3.

--- p1/led.h
+++
@@ -1,6 +0,0 @@
-
-extern void led_on();
-extern void led_off();
-extern void blink_led(unsigned int reps);
-
-
--- p1/utils.h
+++
@@ -1,7 +0,0 @@
-
-extern void PUT32 ( unsigned long, unsigned long );
-extern unsigned long GET32 ( unsigned long );
-extern unsigned long GETPC ( void );
-
-extern void idle();
-
